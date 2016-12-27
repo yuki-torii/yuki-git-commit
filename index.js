737e3cecdc30c3de5e7b916791a79de4bc492369
limichange <limichange@hotmail.com>
2016-12-27 11:52:45 +0800

    update

--- index.js
+++ index.js
@@ -3,52 +3,38 @@ const inquirer = require('inquirer')
 const chalk = require('chalk')
 const figlet = require('figlet')
 const program = require('commander')
-
-clear()
+const questions = require('./questions')
+const spawn = require('child_process').spawn
+const ora = require('ora')
+require('shelljs/global')
+require('console.table')
 
 console.log(
   chalk.green(
-    figlet.textSync('Yuki Git Commit', { horizontalLayout: 'full' })
+    figlet.textSync('YukiGitCommit', { horizontalLayout: 'full' })
   )
 )
 
 program
   .version('0.0.1')
-  .option('-p, --peppers', 'Add peppers')
-  .option('-P, --pineapple', 'Add pineapple')
-  .option('-b, --bbq-sauce', 'Add bbq sauce')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
-  .parse(process.argv);
-
-console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
-if (program.pineapple) console.log('  - pineapple');
-if (program.bbqSauce) console.log('  - bbq');
-console.log('  - %s cheese', program.cheese);
-
-var questions = [
-  {
-    name: 'username',
-    type: 'input',
-    message: 'Enter your Github username or e-mail address:',
-    validate: function( value ) {
-      if (value.length) {
-        return true;
-      } else {
-        return 'Please enter your username or e-mail address';
-      }
-    }
-  },
-  {
-    name: 'test',
-    type: 'rawlist',
-    message: 'type-list',
-    choices: ['a', 'b', 'c'],
-    default: 1
-  }
-];
+  .option('-p, --no-content', 'No content')
+  .parse(process.argv)
 
 inquirer.prompt(questions).then((res) => {
-  console.log('⛩')
-  console.log(res)
-});
+  console.log('\n')
+  console.table(res)
+
+  console.log(`${res.messageTitle}\n${res.messageContent}`)
+
+  if (res.addAllFiles) {
+    exec('git add --all')
+  }
+
+  if (res.pushRightNow) {
+    const spinner = ora('Push Push Push ... ').start()
+
+    exec('git push', () => {
+      spinner.stop()
+    })
+  }
+})
---
+++ questions.js
@@ -0,0 +1,35 @@
+module.exports = [{
+  name: 'addAllFiles',
+  type: 'confirm',
+  message: 'Add all files ?',
+  default: true
+}, {
+  name: 'commitType',
+  type: 'list',
+  message: 'Select your type',
+  choices: [
+    'Upgrading dependencies',
+    'Initial commit',
+    'Improving performance',
+    'Fixing a bug',
+    'Removing code or files',
+    'Improving structure / format of the code',
+    'Critical hotfix'
+  ],
+  default: 0
+}, {
+  name: 'messageTitle',
+  type: 'input',
+  message: 'Git commit message title :',
+  default: 'update'
+}, {
+  name: 'messageContent',
+  type: 'input',
+  message: 'Git commit message content:',
+  default: 'update'
+}, {
+  name: 'pushRightNow',
+  type: 'confirm',
+  message: 'Push right now ?',
+  default: true
+}];
